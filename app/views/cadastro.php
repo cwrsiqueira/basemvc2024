ea5e6a434e833cb2d38af78641f234b5ddf3da02
Carlos Wagner <cwrsiqueira@msn.com>
2024-11-06 06:06:35 -0300

    Tirado os dados fixos de cadastro

--- app/views/cadastro.php
+++ app/views/cadastro.php
@@ -15,11 +15,11 @@
         <form method="post" class="auth-form" action="<?= URL ?>login/cadastrar">
             <div class="mb-3">
                 <label for="username" class="form-label">Nome de Usuário</label>
-                <input type="text" class="form-control form-control-lg <?php @$_SESSION['alert']['campo'] == 'username' ? 'border-danger' : ''; ?>" id="username" name="username" value="<?= @$_SESSION['alert']['old']['username'] ?? 'Carlos'; ?>" placeholder="Digite seu nome de usuário">
+                <input type="text" class="form-control form-control-lg <?php @$_SESSION['alert']['campo'] == 'username' ? 'border-danger' : ''; ?>" id="username" name="username" value="<?= @$_SESSION['alert']['old']['username']; ?>" placeholder="Digite seu nome de usuário">
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control form-control-lg <?php @$_SESSION['alert']['campo'] == 'email' ? 'border-danger' : ''; ?>" id="email" name="email" value="<?= @$_SESSION['alert']['old']['email'] ?? 'cwrsiqueira@msn.com'; ?>" placeholder="Digite seu email">
+                <input type="email" class="form-control form-control-lg <?php @$_SESSION['alert']['campo'] == 'email' ? 'border-danger' : ''; ?>" id="email" name="email" value="<?= @$_SESSION['alert']['old']['email']; ?>" placeholder="Digite seu email">
             </div>
             <div class="text-center mt-4">
                 <button type="submit" class="btn btn-success btn-lg w-100">Cadastrar</button>
